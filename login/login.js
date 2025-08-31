f8bf65d9805489695c6b7aede72a6c5575b3476e
V1GNE5H05 <vigneshkathirmani11@gmail.com>
2025-08-31 14:48:47 +0530

    new

---
+++ login/login.js
@@ -0,0 +1,6 @@
+function login() {
+    var username = document.getElementById("username").textContent
+    var password = document.getElementById("password").textContent
+
+    console.log(username +" "+ password)
+}
